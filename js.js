a71b3f5de0aeb83200f50cc578b19dc430eb7d45
ddobrovodskiy <ddobrovodskiy@gmail.com>
2019-12-10 19:46:21 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,13 @@
+$('.header__slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+
+
+
+    
+      
---
+++ js2.js
@@ -0,0 +1,14 @@
+// $('.news__slider').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+
+  $('.news__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    autoplay: true
+  });
